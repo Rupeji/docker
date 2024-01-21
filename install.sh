9faff5202205627642224898b4ca3ea682d32440
Rupeji <72431133+Rupeji@users.noreply.github.com>
2024-01-21 02:22:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -182,7 +182,6 @@ case $menu_choice in
      	    sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
      	    sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest --logo "https://pi-hosted.com/pi-hosted-logo.png" || error "Failed to run Portainer docker image!"
 	    echo -e "${BOLDCOLOR}${BACKGROUND}Pulsa [Enter] para continuar...${ENDCOLOR}"
-	    echo -e "${BOLDCOLOR}${BACKGROUND}Pulsa [Enter] para continuar...${ENDCOLOR}"
             echo
 	    pausa
             ;;
